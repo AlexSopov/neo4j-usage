677cd95501dc8852e7f54ecf61a97c1f6b54221b
Alex Sopov <sopov.alex.a@gmail.com>
2017-12-16 01:34:01 +0200

    Add interactivity with neo4j logic.

---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+    public static void main(String[] args) {
+        try (SocialNetworkInteractivity socialNetworkInteractivity = new SocialNetworkInteractivity()) {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
